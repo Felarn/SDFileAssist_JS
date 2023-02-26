8ef029c140e748c25dc5f06138d0a51885746338
Александр Шмелев <120136549+Felarn@users.noreply.github.com>
2023-02-26 19:15:46 +0300

    добавлено распознавание файл/папка

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
+import fs from 'fs';
 import setConfig from "./utils/setConfig.js";
 const cfg = setConfig();
+
+const contentList=fs.readdirSync(cfg.targetPath)
+console.log(contentList);
+const tree = {}
+const stats = [];
+for (let itemName of contentList) {
+    console.log(itemName);
+    const itemFullPath = cfg.targetPath+'/'+itemName;
+    console.log(itemFullPath);
+    const isFile = fs.statSync(itemFullPath).isFile();
+    tree[itemName] = {isFile}
+}
+console.log(tree);
